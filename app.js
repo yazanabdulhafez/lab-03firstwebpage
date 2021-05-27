72fa1a4399033d8e1cd89ceb34b6713d3688e4dd
YazanAlkarabshe <>
2021-05-27 20:31:50 +0000

    changes in js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ alert("WELLCOM TO OUR WEBSITE")
 var txt;
 var r = confirm("DO YOU WANT TO CHOOSE  THE GREAT WONDER?");
 if (r == true) {
- var userFav=prompt("what wonder you think its the greatest(in uppercase please)")
+ var userFav=prompt("what wonder you think its the greatest(in uppercase please)Hint:PETRA")
 if (userFav==="PETRA"){
   alert("I HOPE THAT YOU VISIT THIS SITE SOON ")
   document.write('<img src="https://img.theculturetrip.com/1440x807/smart/wp-content/uploads/2020/11/tahw71-1.jpg " width="100%"/> ')
