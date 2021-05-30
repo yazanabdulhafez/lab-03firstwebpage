beb3fb5c057c6199adf641130056da6b5e34e022
YazanAlkarabshe <>
2021-05-30 12:09:55 +0000

    add new java script

--- app.js
+++ app.js
@@ -1,21 +1,36 @@
+
+// var txt;
+// var r = confirm("DO YOU WANT TO CHOOSE  THE GREAT WONDER?");
+// if (r == true) {
+//  var userFav=prompt("what wonder you think its the greatest(in uppercase please)Hint:PETRA")
+// if (userFav==="PETRA"){
+//   alert("I HOPE THAT YOU VISIT THIS SITE SOON ")
+//   document.write('<img src="https://img.theculturetrip.com/1440x807/smart/wp-content/uploads/2020/11/tahw71-1.jpg " width="100%"/> ')
+
+// }
+// else if (userFav!=="PETRA"){
+//   alert("THE "+userFav+" IS GREAT WONDER")
+
+// }
+// }
+
+//  else {
+//   txt = "You pressed Cancel!"
+//   alert(txt)
+// // }
+// var userName =prompt('what is your name')
+// alert('welcome to our website '+userName)
+
+var userAge=prompt('please input your age?')
+while (userAge<18){
+  userAge=prompt('please input your age again ?')
+}
 alert("WELLCOM TO OUR WEBSITE")
-var txt;
-var r = confirm("DO YOU WANT TO CHOOSE  THE GREAT WONDER?");
-if (r == true) {
- var userFav=prompt("what wonder you think its the greatest(in uppercase please)Hint:PETRA")
-if (userFav==="PETRA"){
-  alert("I HOPE THAT YOU VISIT THIS SITE SOON ")
-  document.write('<img src="https://img.theculturetrip.com/1440x807/smart/wp-content/uploads/2020/11/tahw71-1.jpg " width="100%"/> ')
 
-}
-else if (userFav!=="PETRA"){
-  alert("THE "+userFav+" IS GREAT WONDER")
+var userRating=prompt('please rate our website from 1 to 5?')
+for (let i=0 ; i<userRating ; i++)
+{
+  document.write('<img id=jsimg src="https://affaso.com/wp-content/uploads/2020/06/5-point-stars-png-star-icon-flat-11562958768wpf63hu4tq.png"/>')
 
-}
-}
-
- else {
-  txt = "You pressed Cancel!"
-  alert(txt)
 }
 
