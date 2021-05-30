45f8f96df3b78cdb1963459cd1d47704590e1222
YazanAlkarabshe <>
2021-05-30 13:55:54 +0000

    changes in js

--- app.js
+++ app.js
@@ -26,13 +26,17 @@ while (userAge<18){
   userAge=prompt('please input your age again ?')
 }
 alert("WELLCOM TO OUR WEBSITE")
-
+var r = confirm("DO YOU WANT TO VOTE?");
+ if (r == true) {
 var userRating=prompt('please rate our website from 1 to 5?')
-for (let i=0 ; i<userRating ; i++)
+for (let i=0 ; i<userRating && i<5 ; i++)
 {
   document.write('<img id=jsimg src="https://affaso.com/wp-content/uploads/2020/06/5-point-stars-png-star-icon-flat-11562958768wpf63hu4tq.png"/>')
   //console.log(i+1)
   document.write(i+1)
 
 }
-
+ }
+ else{
+alert('you did not rated our web site')
+ }
